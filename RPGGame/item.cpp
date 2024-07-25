b0794e3b9b6068906209ac78886183cb0189e020
oracle312 <oracle312@naver.com>
2024-07-26 00:09:06 +0900

    feat: drop_item ing..

---
+++ RPGGame/item.cpp
@@ -0,0 +1,113 @@
+#include "item.h"
+#include <iostream>
+
+using namespace std;
+
+// **********************
+//         Item           
+// **********************
+
+Item::Item()
+{
+	int randValue = rand() % 100;
+
+	if (randValue < 50)
+	{
+		_rarity = IR_Normal;
+	}
+	else if (randValue < 80)
+	{
+		_rarity = IR_Rare;
+	}
+	else
+	{
+		_rarity = IR_Unique;
+	}
+}
+
+Item::~Item()
+{
+
+}
+
+void Item::PrintInfo()
+{
+	switch (_rarity)
+	{
+	case IR_Normal:
+		cout << "[희귀도] 일반" << endl;
+		break;
+	case IR_Rare:
+		cout << "[희귀도] 레어" << endl;
+		break;
+	case IR_Unique:
+		cout << "[희귀도] 유니크" << endl;
+		break;
+	}
+}
+
+// **********************
+//         Weapon           
+// **********************
+
+Weapon::Weapon()
+{
+	switch (_rarity)
+	{
+	case IR_Normal:
+		_damage = 1 + rand() % 5;
+		break;
+	case IR_Rare:
+		_damage = 5 + rand() % 5;
+		break;
+	case IR_Unique:
+		_damage = 10 + rand() % 5;
+		break;
+	}
+}
+
+Weapon::~Weapon()
+{
+}
+
+void Weapon::PrintInfo()
+{
+	cout << "********************" << endl;
+	cout << "[아이템 타입] : 무기" << endl;
+	cout << "[공격력] : " << _damage << endl;
+	Item::PrintInfo();
+	cout << "********************" << endl;
+}
+
+// **********************
+//         Armor           
+// **********************
+
+Armor::Armor()
+{
+	switch (_rarity)
+	{
+	case IR_Normal:
+		_defence = 1 + rand() % 5;
+		break;
+	case IR_Rare:
+		_defence = 5 + rand() % 5;
+		break;
+	case IR_Unique:
+		_defence = 10 + rand() % 5;
+		break;
+	}
+}
+
+Armor::~Armor()
+{
+}
+
+void Armor::PrintInfo()
+{
+	cout << "********************" << endl;
+	cout << "[아이템 타입] : 방어구" << endl;
+	cout << "[방어력] : " << _defence << endl;
+	Item::PrintInfo();
+	cout << "********************" << endl;
+}
---
+++ RPGGame/item.h
@@ -0,0 +1,58 @@
+#pragma once
+
+// Item
+// - Weapon
+// - Armor
+// - Consumable
+
+enum ItemRarity
+{
+	IR_None,
+	IR_Normal,
+	IR_Rare,
+	IR_Unique
+};
+
+class Item
+{
+public:
+	Item();
+	virtual ~Item();
+
+	virtual void PrintInfo();
+
+protected:
+	int _itemId = 0;
+	int _itemCount = 0;
+	ItemRarity _rarity = IR_Normal;
+};
+
+class Weapon : public Item
+{
+public:
+	Weapon();
+	virtual ~Weapon();
+
+	virtual void PrintInfo() override;
+
+	void SetDamage(int damage) { _damage = damage; }
+	int GetDamage() { return _damage; }
+
+private:
+	int _damage = 0;
+};
+
+class Armor : public Item
+{
+public:
+	Armor();
+	virtual ~Armor();
+
+	virtual void PrintInfo() override;
+
+	void SetDefence(int defence) { _defence = defence; }
+	int GetDamage() { return _defence; }
+
+private:
+	int _defence = 0;
+};
